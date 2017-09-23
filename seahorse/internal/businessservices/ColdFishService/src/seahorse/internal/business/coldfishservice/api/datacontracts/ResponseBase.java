6ad02f7a656ace7c758d406ff1e4c5ca382ebcd4
sajanmje <jess.sajan@gmail.com>
2017-09-22 22:44:44 -0400

    Cold fish

---
+++ seahorse/internal/businessservices/ColdFishService/src/seahorse/internal/business/coldfishservice/api/datacontracts/ResponseBase.java
@@ -0,0 +1,58 @@
+/**
+ * 
+ */
+package seahorse.internal.business.coldfishservice.api.datacontracts;
+
+import java.util.ArrayList;
+import java.util.List;
+
+/**
+ * @author admin
+ *
+ */
+public class ResponseBase {
+
+	private  List<ResultMessage> messages;
+	private String status;
+
+	/**
+	 * @return the _resultMessage
+	 */
+	public List<ResultMessage> getresultMessage() {
+		return messages;
+	}
+
+	/**
+	 * @param _resultMessage the _resultMessage to set
+	 */
+	public void setresultMessage(List<ResultMessage> resultMessages) {
+		this.messages = resultMessages;
+	}
+
+	/**
+	 * @return the resultstatus
+	 */
+	public String getResultStatus() {
+		return status;
+	}
+
+	/**
+	 * @param resultstatus the resultstatus to set
+	 */
+	public void setResultStatus(String resultstatus) {
+		this.status = resultstatus;
+	}
+	
+	/**
+	 * @param _resultMessage the _resultMessage to set
+	 */
+	public void setresultMessage(ResultMessage resultMessage) {
+		if (this.messages == null) {
+			this.messages = new ArrayList<>();
+		}
+		this.messages.add(resultMessage);
+	}
+
+
+	
+}
