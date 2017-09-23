8755cbee544032a43a097d301dc7de4c1bdd80a3
sajanmje <jess.sajan@gmail.com>
2017-09-22 23:07:40 -0400

    cf

--- seahorse/internal/businessservices/ColdFishService/src/seahorse/internal/business/coldfishservice/ColdFishService.java
+++ seahorse/internal/businessservices/ColdFishService/src/seahorse/internal/business/coldfishservice/ColdFishService.java
@@ -3,15 +3,33 @@
  */
 package seahorse.internal.business.coldfishservice;
 
+import org.apache.logging.log4j.Logger;
+
 import seahorse.internal.business.coldfishservice.datacontracts.IncomeTypeMessageEntity;
 import seahorse.internal.business.coldfishservice.datacontracts.IncomeTypeResponseMessageEntity;
+import seahorse.internal.business.coldfishservice.validators.IColdFishServiceValidator;
+import seahorse.internal.business.coldfishservice.verifiers.IColdFishServiceVerifier;
+import seahorse.internal.business.shared.aop.InjectLogger;
 
 /**
  * @author sajanmje
  *
  */
 public class ColdFishService implements IColdFishService {
+	private IColdFishServiceMapper coldFishServiceMapper;
+	private IColdFishServiceVerifier coldFishServiceVerifier;
+	private IColdFishServiceValidator coldFishServiceValidator;
+	
+	@InjectLogger  Logger logger;
 
+	public ColdFishService(IColdFishServiceMapper coldFishServiceMapper,
+			IColdFishServiceVerifier coldFishServiceVerifier,
+			IColdFishServiceValidator coldFishServiceValidator)
+	{
+		this.coldFishServiceMapper=coldFishServiceMapper;
+		this.coldFishServiceVerifier=coldFishServiceVerifier;
+		this.coldFishServiceValidator=coldFishServiceValidator;
+	}
 	
 	public IncomeTypeResponseMessageEntity CreateIncomeType(IncomeTypeMessageEntity incomeTypeMessageEntity) {
 		// TODO Auto-generated method stub
