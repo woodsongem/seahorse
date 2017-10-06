7c8d72ad556e591af522d613803183de5eb304ea
sajanmje <jess.sajan@gmail.com>
2017-10-05 23:16:24 -0400

    CF

---
+++ seahorse/internal/businessservices/ColdFishService/src/seahorse/internal/business/coldfishservice/dal/datacontracts/BaseDAO.java
@@ -0,0 +1,79 @@
+/**
+ * 
+ */
+package seahorse.internal.business.coldfishservice.dal.datacontracts;
+
+import java.util.Date;
+
+/**
+ * @author sajanmje
+ *
+ */
+public class BaseDAO {
+	
+	private String createdBy;
+	private String modifiedBy;
+	private Date createdDate;
+	private Date modifiedDate;	
+	private String ipAddress;
+	/**
+	 * @return the ipAddress
+	 */
+	public String getIpAddress() {
+		return ipAddress;
+	}
+	/**
+	 * @param ipAddress the ipAddress to set
+	 */
+	public void setIpAddress(String ipAddress) {
+		this.ipAddress = ipAddress;
+	}
+	/**
+	 * @return the modifiedDate
+	 */
+	public Date getModifiedDate() {
+		return modifiedDate;
+	}
+	/**
+	 * @param modifiedDate the modifiedDate to set
+	 */
+	public void setModifiedDate(Date modifiedDate) {
+		this.modifiedDate = modifiedDate;
+	}
+	/**
+	 * @return the createdDate
+	 */
+	public Date getCreatedDate() {
+		return createdDate;
+	}
+	/**
+	 * @param createdDate the createdDate to set
+	 */
+	public void setCreatedDate(Date createdDate) {
+		this.createdDate = createdDate;
+	}
+	/**
+	 * @return the modifiedBy
+	 */
+	public String getModifiedBy() {
+		return modifiedBy;
+	}
+	/**
+	 * @param modifiedBy the modifiedBy to set
+	 */
+	public void setModifiedBy(String modifiedBy) {
+		this.modifiedBy = modifiedBy;
+	}
+	/**
+	 * @return the createdBy
+	 */
+	public String getCreatedBy() {
+		return createdBy;
+	}
+	/**
+	 * @param createdBy the createdBy to set
+	 */
+	public void setCreatedBy(String createdBy) {
+		this.createdBy = createdBy;
+	}
+}
