320a6d79ccc0618e034c6a11c6489762f38c8de8
sajanmje <jess.sajan@gmail.com>
2017-10-16 23:49:43 -0400

    income type

---
+++ seahorse/internal/businessservices/ColdFishService/src/seahorse/internal/business/coldfishservice/api/datacontracts/IncomeType.java
@@ -0,0 +1,108 @@
+/**
+ * 
+ */
+package seahorse.internal.business.coldfishservice.api.datacontracts;
+
+/**
+ * @author sajanmje
+ *
+ */
+public class IncomeType extends Base {
+
+	private String id;
+	private String name;
+	private String userId;
+	private String description;
+	private String category;
+	private String Status;
+
+	/**
+	 * @return the name
+	 */
+	public String getName() {
+		return name;
+	}
+
+	/**
+	 * @param name
+	 *            the name to set
+	 */
+	public void setName(String name) {
+		this.name = name;
+	}
+
+	/**
+	 * @return the userId
+	 */
+	public String getUserId() {
+		return userId;
+	}
+
+	/**
+	 * @param userId
+	 *            the userId to set
+	 */
+	public void setUserId(String userId) {
+		this.userId = userId;
+	}
+
+	/**
+	 * @return the description
+	 */
+	public String getDescription() {
+		return description;
+	}
+
+	/**
+	 * @param description
+	 *            the description to set
+	 */
+	public void setDescription(String description) {
+		this.description = description;
+	}
+
+	/**
+	 * @return the category
+	 */
+	public String getCategory() {
+		return category;
+	}
+
+	/**
+	 * @param category
+	 *            the category to set
+	 */
+	public void setCategory(String category) {
+		this.category = category;
+	}
+
+	/**
+	 * @return the id
+	 */
+	public String getId() {
+		return id;
+	}
+
+	/**
+	 * @param id
+	 *            the id to set
+	 */
+	public void setId(String id) {
+		this.id = id;
+	}
+
+	/**
+	 * @return the status
+	 */
+	public String getStatus() {
+		return Status;
+	}
+
+	/**
+	 * @param status the status to set
+	 */
+	public void setStatus(String status) {
+		Status = status;
+	}
+
+}
