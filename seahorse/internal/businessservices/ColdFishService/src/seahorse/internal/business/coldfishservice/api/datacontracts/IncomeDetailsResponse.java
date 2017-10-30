cde4ee2385b5da761b7a2edf6102e45a64ca0607
sajanmje <jess.sajan@gmail.com>
2017-10-30 00:03:08 -0400

    CF

---
+++ seahorse/internal/businessservices/ColdFishService/src/seahorse/internal/business/coldfishservice/api/datacontracts/IncomeDetailsResponse.java
@@ -0,0 +1,27 @@
+/**
+ * 
+ */
+package seahorse.internal.business.coldfishservice.api.datacontracts;
+
+/**
+ * @author sajanmje
+ *
+ */
+public class IncomeDetailsResponse extends ResponseBase {
+
+	private String id;
+
+	/**
+	 * @return the id
+	 */
+	public String getId() {
+		return id;
+	}
+
+	/**
+	 * @param id the id to set
+	 */
+	public void setId(String id) {
+		this.id = id;
+	}
+}
