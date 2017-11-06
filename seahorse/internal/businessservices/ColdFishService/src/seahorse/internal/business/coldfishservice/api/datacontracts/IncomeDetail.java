d741a977d2e45b9bf29e75c35755f6367416a43c
sajanmje <jess.sajan@gmail.com>
2017-11-05 23:35:27 -0500

    CF

---
+++ seahorse/internal/businessservices/ColdFishService/src/seahorse/internal/business/coldfishservice/api/datacontracts/IncomeDetail.java
@@ -0,0 +1,116 @@
+/**
+ * 
+ */
+package seahorse.internal.business.coldfishservice.api.datacontracts;
+
+/**
+ * @author sajanmje
+ *
+ */
+public class IncomeDetail extends Base {
+	private String id;
+	private String name;
+	private String userId;
+	private String description;	
+	private String category;
+	private String incomeTypeName;
+	private double amount;
+	private String status;
+	
+	/**
+	 * @return the id
+	 */
+	public String getId() {
+		return id;
+	}
+	/**
+	 * @param id the id to set
+	 */
+	public void setId(String id) {
+		this.id = id;
+	}
+	/**
+	 * @return the name
+	 */
+	public String getName() {
+		return name;
+	}
+	/**
+	 * @param name the name to set
+	 */
+	public void setName(String name) {
+		this.name = name;
+	}
+	/**
+	 * @return the userId
+	 */
+	public String getUserId() {
+		return userId;
+	}
+	/**
+	 * @param userId the userId to set
+	 */
+	public void setUserId(String userId) {
+		this.userId = userId;
+	}
+	/**
+	 * @return the description
+	 */
+	public String getDescription() {
+		return description;
+	}
+	/**
+	 * @param description the description to set
+	 */
+	public void setDescription(String description) {
+		this.description = description;
+	}
+	/**
+	 * @return the category
+	 */
+	public String getCategory() {
+		return category;
+	}
+	/**
+	 * @param category the category to set
+	 */
+	public void setCategory(String category) {
+		this.category = category;
+	}
+	/**
+	 * @return the amount
+	 */
+	public double getAmount() {
+		return amount;
+	}
+	/**
+	 * @param amount the amount to set
+	 */
+	public void setAmount(double amount) {
+		this.amount = amount;
+	}
+	/**
+	 * @return the incomeTypeId
+	 */
+	public String getIncomeTypeName() {
+		return incomeTypeName;
+	}
+	/**
+	 * @param incomeTypeId the incomeTypeId to set
+	 */
+	public void setIncomeTypeName(String incomeTypeName) {
+		this.incomeTypeName = incomeTypeName;
+	}
+	/**
+	 * @return the status
+	 */
+	public String getStatus() {
+		return status;
+	}
+	/**
+	 * @param status the status to set
+	 */
+	public void setStatus(String status) {
+		this.status = status;
+	}
+}
