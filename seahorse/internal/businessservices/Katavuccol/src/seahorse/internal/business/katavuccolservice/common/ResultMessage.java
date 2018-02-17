abee1f64bf4f8eebc5c53d3a2d816294b7955069
sajanmje <jess.sajan@gmail.com>
2018-02-17 00:45:07 -0500

    missing files

---
+++ seahorse/internal/businessservices/Katavuccol/src/seahorse/internal/business/katavuccolservice/common/ResultMessage.java
@@ -0,0 +1,38 @@
+/**
+ * 
+ */
+package seahorse.internal.business.katavuccolservice.common;
+
+/**
+ * @author sajanmje
+ *
+ */
+public class ResultMessage {
+
+	private String parameter;
+	private String errorCode;
+	/**
+	 * @return the parameter
+	 */
+	public String getParameter() {
+		return parameter;
+	}
+	/**
+	 * @param parameter the parameter to set
+	 */
+	public void setParameter(String parameter) {
+		this.parameter = parameter;
+	}
+	/**
+	 * @return the errorCode
+	 */
+	public String getErrorCode() {
+		return errorCode;
+	}
+	/**
+	 * @param errorCode the errorCode to set
+	 */
+	public void setErrorCode(String errorCode) {
+		this.errorCode = errorCode;
+	}
+}
