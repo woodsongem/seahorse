5e298eea057ff4cac377af4a29e1f0b1e6c37b94
sajanmje <jess.sajan@gmail.com>
2018-02-18 01:08:36 -0500

    missed files check-in

---
+++ seahorse/internal/businessservices/Katavuccol/src/seahorse/internal/business/katavuccolservice/common/datacontracts/Result.java
@@ -0,0 +1,44 @@
+/**
+ * 
+ */
+package seahorse.internal.business.katavuccolservice.common.datacontracts;
+
+import java.util.List;
+
+/**
+ * @author sajanmje
+ *
+ */
+public class Result {
+
+	private ResultStatus resultStatus;
+	private List<ResultMessage> resultMessages;
+
+	/**
+	 * @return the resultStatus
+	 */
+	public ResultStatus getResultStatus() {
+		return resultStatus;
+	}
+
+	/**
+	 * @param resultStatus the resultStatus to set
+	 */
+	public void setResultStatus(ResultStatus resultStatus) {
+		this.resultStatus = resultStatus;
+	}
+
+	/**
+	 * @return the resultMessages
+	 */
+	public List<ResultMessage> getResultMessages() {
+		return resultMessages;
+	}
+
+	/**
+	 * @param resultMessages the resultMessages to set
+	 */
+	public void setResultMessages(List<ResultMessage> resultMessages) {
+		this.resultMessages = resultMessages;
+	}
+}
