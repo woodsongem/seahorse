5a1c2f99ba178020ad83ffd27558e284627b1fcb
sajanmje <jess.sajan@gmail.com>
2018-05-08 22:39:44 -0400

    missed values

---
+++ seahorse/internal/businessservices/Katavuccol/src/seahorse/internal/business/katavuccolservice/datacontracts/CredentialValueDetail.java
@@ -0,0 +1,30 @@
+/**
+ * 
+ */
+package seahorse.internal.business.katavuccolservice.datacontracts;
+
+import seahorse.internal.business.katavuccolservice.common.datacontracts.BaseMessageEntity;
+
+/**
+ * @author sajanmje
+ *
+ */
+public class CredentialValueDetail extends BaseMessageEntity {
+	
+	private String value;
+
+	/**
+	 * @return the value
+	 */
+	public String getValue() {
+		return value;
+	}
+
+	/**
+	 * @param value the value to set
+	 */
+	public void setValue(String value) {
+		this.value = value;
+	}
+
+}
