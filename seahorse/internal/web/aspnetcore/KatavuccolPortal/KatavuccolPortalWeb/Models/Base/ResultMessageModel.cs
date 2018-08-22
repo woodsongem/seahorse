a7bea981eb0a3efdd52c8fed6922752e7bf2383e
woodsongem <jess.sajan@gmail.com>
2018-08-22 19:30:02 -0400

    Delete ResultMessageModel.cs

--- seahorse/internal/web/aspnetcore/KatavuccolPortal/KatavuccolPortalWeb/Models/Base/ResultMessageModel.cs
+++
@@ -1,9 +0,0 @@
-﻿namespace KatavuccolPortalWeb.Models.Base
-{
-    public class ResultMessageModel
-    {
-        public string ErrorCode { get; set; }
-        public string ErrorMessage { get; set; }
-        public string Parameter { get; set; }
-    }
-}
