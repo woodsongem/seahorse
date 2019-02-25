7f58e56570b1f3484b66dd075ab5a1c04f1cebb6
woodson <jess.sajan@gmail.com>
2019-02-24 22:16:34 -0500

    OPen API changes

---
+++ seahorse/External/businessservices/ColdFishServiceOpenApiWebApi/Startup.cs
@@ -0,0 +1,66 @@
+using ColdFishServiceOpenApi.AuthenticationService.Mappers;
+using ColdFishServiceOpenApi.AuthenticationService.PostProcessors;
+using ColdFishServiceOpenApi.AuthenticationService.Processors;
+using ColdFishServiceOpenApi.AuthenticationService.Repository;
+using ColdFishServiceOpenApi.AuthenticationService.Services;
+using ColdFishServiceOpenApi.AuthenticationService.Validators;
+using ColdFishServiceOpenApi.AuthenticationService.Verifiers;
+using Microsoft.AspNetCore.Builder;
+using Microsoft.AspNetCore.Hosting;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.Extensions.Configuration;
+using Microsoft.Extensions.DependencyInjection;
+
+namespace ColdFishServiceOpenApiWebApi
+{
+    public class Startup
+    {
+        public Startup(IConfiguration configuration)
+        {
+            Configuration = configuration;
+        }
+
+        public IConfiguration Configuration { get; }
+
+        // This method gets called by the runtime. Use this method to add services to the container.
+        public void ConfigureServices(IServiceCollection services)
+        {
+            services.AddMvc().SetCompatibilityVersion(CompatibilityVersion.Version_2_2);
+
+            // Register the Swagger services
+            services.AddSwaggerDocument();
+
+            #region Authentication Service
+
+            services.AddSingleton<IAuthenticationServiceMapper, AuthenticationServiceMapper>();
+            services.AddSingleton<IAuthenticationServicePostProcessors, AuthenticationServicePostProcessors>();
+            services.AddSingleton<IAuthenticationServiceProcessors, AuthenticationServiceProcessors>();
+            services.AddSingleton<IAuthenticationService, AuthenticationService>();
+            services.AddSingleton<IAuthenticationServiceValidator, AuthenticationServiceValidator>();
+            services.AddSingleton<IAuthenticationServiceVerifier, AuthenticationServiceVerifier>();
+            services.AddSingleton<IAuthenticationServiceRepository, AuthenticationServiceRepository>();
+
+            #endregion
+        }
+
+        // This method gets called by the runtime. Use this method to configure the HTTP request pipeline.
+        public void Configure(IApplicationBuilder app, IHostingEnvironment env)
+        {
+            if (env.IsDevelopment())
+            {
+                app.UseDeveloperExceptionPage();
+            }
+            else
+            {
+                // The default HSTS value is 30 days. You may want to change this for production scenarios, see https://aka.ms/aspnetcore-hsts.
+                app.UseHsts();
+            }
+
+            // Register the Swagger generator and the Swagger UI middlewares
+            app.UseSwagger();
+            app.UseSwaggerUi3();
+            app.UseHttpsRedirection();
+            app.UseMvc();
+        }
+    }
+}
